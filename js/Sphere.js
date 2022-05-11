57d52bca4f167997b85aeea6f40b98b50793b3a1
DESKTOP-UAD63F6\Eva <evaradu@hotmail.fr>
2022-05-11 11:10:30 +0200

    init

---
+++ js/Sphere.js
@@ -0,0 +1,50 @@
+export default class Sphere {
+    constructor(sphereMesh, id, scaling, scene, texture) {
+        this.sphereMesh = sphereMesh;
+        this.id = id;
+        this.scene = scene;
+        this.scaling = scaling;
+
+        let sphereMaterial = new BABYLON.StandardMaterial("sphereMaterial" , scene);
+        
+        //var sphereMaterial = new BABYLON.PBRMaterial("sphereMaterial", this.scene);
+        //sphereMaterial.subSurface.isRefractionEnabled = true;
+
+        sphereMaterial.glossiness = 3;
+        sphereMaterial.metallic = 5;
+        sphereMaterial.roughness = 0.5;    
+        sphereMaterial.specularPower = 8;
+        
+        
+        if (texture != null) {
+            sphereMaterial.diffuseTexture = new BABYLON.Texture(texture, this.scene); 
+        }
+        
+        //sphereMaterial.diffuseTexture.level = 2;
+
+        sphereMaterial.alpha = Math.random()*(1-0.7+1)+0.7; // niveau de transparence
+        
+        sphereMaterial.emissiveColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        sphereMaterial.reflectivityColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        sphereMaterial.reflectionColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        sphereMaterial.albedoColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+
+        sphereMesh.material = sphereMaterial;
+                    
+        sphereMesh.Sphere = this;
+
+        //this.sphereMesh.scaling = new BABYLON.Vector3(0.2  , 0.2, 0.2);
+        sphereMesh.position.x = Math.floor(Math.random()*(300-(-300)+1)+(-300));
+        sphereMesh.position.z = Math.floor(Math.random()*(300-(-300)+1)+(-300));
+        sphereMesh.position.y = 50;
+
+        sphereMesh.checkCollisions = true;
+        //sphereMesh.showBoundingBox = true;
+
+      
+    }
+
+
+
+    
+}
---
+++ js/SuperBall.js
@@ -0,0 +1,60 @@
+import Sphere from "./Sphere.js";
+
+export default class sphereMesh extends Sphere {
+    constructor(sphereMesh, id, scaling, scene, texture){
+        super(sphereMesh,id,scaling,scene,texture);
+        sphereMesh.position.x = 0;
+        //sphereMesh.position.y = 5;     
+        sphereMesh.position.z = 0;
+        sphereMesh.speed = 4;
+        sphereMesh.frontVector = new BABYLON.Vector3(0, 0, 1);
+        
+        /*sphereMesh.onCollideObservable.add(function () {
+            alert("collideObservable work!");
+        });*/
+
+    }
+
+    updateParticles(){
+            // Create a particle system
+            var particleSystem = new BABYLON.ParticleSystem("particles", 2000, this.scene);
+    
+            //Texture of each particle
+            particleSystem.particleTexture = new BABYLON.Texture("textures/flare.png", this.scene);
+    
+            // Where the particles come from
+            particleSystem.emitter = this.sphereMesh.position;
+    
+            // Colors of all particles
+            particleSystem.color1 = new BABYLON.Color4(0.84, 0.17, 0.36);
+            particleSystem.color2 = new BABYLON.Color4(0.2, 1, 0.91, 0.56);
+            particleSystem.colorDead = new BABYLON.Color4(0, 0, 0.2, 0.0);
+    
+            // Size of each particle (random between...
+            particleSystem.minSize = 0.1;
+            particleSystem.maxSize = 0.5;
+    
+            // Life time of each particle (random between...
+            particleSystem.minLifeTime = 0.05;
+            particleSystem.maxLifeTime = 0.1;
+    
+            // Emission rate
+            particleSystem.emitRate = 1000;
+    
+    
+            /******* Emission Space ********/
+            particleSystem.createSphereEmitter(5.5);
+    
+    
+            // Speed
+            particleSystem.minEmitPower = 1;
+            particleSystem.maxEmitPower = 7;
+            particleSystem.updateSpeed = 0.005;
+    
+            particleSystem.targetStopDuration = 0.07;
+    
+            // Start the particle system
+            particleSystem.start();
+        }
+    }
+
